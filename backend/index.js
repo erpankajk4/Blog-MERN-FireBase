4d12e1d1af7d36c9f2f678aeabaebd00fdda4bc5
pankaj <er.pankaj,k4@gmail.com>
2024-03-07 00:07:43 +0530

    - Creating userSchema, user and auth route and controller, installing and use bycrptjs - Creating a middleware and its function to handle errors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
@@ -15,6 +17,25 @@ mongoose
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
 app.listen(8000, () => {
-  console.log('Server is running on port 8000!');
+  console.log("Server is running on port 8000!");
+});
+
+
+// Routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+// Middleware for errors handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
