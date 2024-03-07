b0162797ae0e492fc0a0c451b3725bb2a29fd747
pankaj <er.pankaj,k4@gmail.com>
2024-03-08 00:31:11 +0530

    PrivateRoute to DashBoard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,18 +8,21 @@ import Projects from "./pages/Projects";
 import SignUp from "./pages/SignUp";
 import Search from "./pages/Search";
 import Header from "./components/Header";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
