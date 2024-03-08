c1573629d1126c3ab5cd1f2f9c670a5d4c03be3e
pankaj <er.pankaj,k4@gmail.com>
2024-03-08 16:43:36 +0530

    adding update post functionality at frontend and backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 function App() {
   return (
     <BrowserRouter>
@@ -27,6 +28,7 @@ function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
