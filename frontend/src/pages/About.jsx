acc30ead6b3cccd61311e35c333b3e1aa30f9f1a
pankaj <er.pankaj,k4@gmail.com>
2024-03-13 23:59:01 +0530

    Creating About and Project Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Developer
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Hello there! I'm Pankaj Kumar, a seasoned professional with a rich
+              background in healthcare business and health consulting. With four
+              years of experience in the healthcare industry and three months as
+              a web developer, I bring a unique blend of skills encompassing
+              customer service, networking, and problem-solving.
+            </p>
+
+            <p>
+              Currently, I am on an exciting journey as a MERN stack Full Stack
+              Web Developer, where I merge my analytical skills and business
+              acumen with evolving technological proficiency. My recent role at
+              Sprite Genix involves collaborative web development, ensuring
+              seamless user experiences, and deploying websites on cPanel.
+            </p>
+
+            <p>
+              Beyond my professional pursuits, I am passionate about exploring
+              holistic well-being through Ayurveda, as evidenced by my
+              establishment and management of a healthcare business specializing
+              in Ayurvedic health consultancy. I am committed to driving
+              innovation and impactful change, making a fluid shift into the IT
+              sector to contribute to the dynamic world of technology.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { BsGithub } from "react-icons/bs";
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <>
+      <div className="min-h-screen w-full mx-auto flex justify-center items-center flex-col gap-6">
+        <h1 className="text-3xl font-semibold">Projects</h1>
+        <p className="text-md text-gray-500">
+          Checkout my Github Repository if you want to see my more projects.
+        </p>
+        <Link to="https://github.com/erpankajk4">
+          <Button outline gradientDuoTone="purpleToPink">
+            <BsGithub className="mr-3 h-6 w-6" />
+            GitHub
+          </Button>
+        </Link>
+        <div className="p-3 bg-amber-100 dark:bg-slate-700 w-full">
+          <div className="max-w-2xl mx-auto w-full">
+            <CallToAction />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
