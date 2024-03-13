77a4f1b07314df22d5585382fedfa4c89278b66c
pankaj <er.pankaj,k4@gmail.com>
2024-03-13 22:58:18 +0530

    creating home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getPosts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Our Latest Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <div className="max-w-2xl mx-auto w-full">
+        <CallToAction />
+        </div>
+      </div>
+    </div>
+  );
 }
